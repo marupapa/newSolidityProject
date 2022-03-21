35759ccaddd3a4f20500c58b522fbc9606aa7c16
HYUK LEE <ruldes76@gmail.com>
2022-03-21 21:52:27 +0900

    test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const Shipping = artifacts.require("Shipping");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+};
+
+module.exports = function (deployer) {
+  deployer.deploy(Shipping);
 };
